f855db7aca84af1905c2c352134d4880904696aa
Ferry Utomo <huferry@gmail.com>
2020-05-22 18:47:13 +0200

    Updates

---
+++ precache-manifest.bdd293bd6efb483ad7f88910ce5d5ebe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a4f3bfa3c23df06b776258c2a5bfd8b",
+    "url": "/bible-search/index.html"
+  },
+  {
+    "revision": "f2ee82fc0f3c42cfc631",
+    "url": "/bible-search/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "f30cffc5d18f97863fee",
+    "url": "/bible-search/static/css/main.a57a9fbf.chunk.css"
+  },
+  {
+    "revision": "f2ee82fc0f3c42cfc631",
+    "url": "/bible-search/static/js/2.65e76ab6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bible-search/static/js/2.65e76ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30cffc5d18f97863fee",
+    "url": "/bible-search/static/js/main.ffbdf563.chunk.js"
+  },
+  {
+    "revision": "808b40b4a4ef4d050d0a",
+    "url": "/bible-search/static/js/runtime-main.405e5bd7.js"
+  }
+]);
